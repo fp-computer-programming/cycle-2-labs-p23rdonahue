caa16ccb39d30f6037b7f55d70e4b2d40b3ba06a
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-09-14 09:15:33 -0400

    lab 2-1 complete

---
+++ lab_2-1.py
@@ -0,0 +1,23 @@
+print(123 +321 -321)
+
+print(20+20)
+
+print(2020-4)
+
+print(20 + 20)
+
+print(2*4*6*8)
+
+print(9/5)
+
+print(9//5)
+
+#print(13/0)
+
+print(3 % 10)
+
+print(2 ** 3)
+
+print(2 * 2 * 2)
+
+#print(0/0)
