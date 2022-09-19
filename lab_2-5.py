a415477621ce704112112cbfbb79d1ad75079448
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-09-19 12:03:20 -0400

    lab 2-5 completed

---
+++ lab_2-5.py
@@ -0,0 +1,44 @@
+# Author: RED 9/19
+
+x=21
+y=21
+z=21
+w=21
+t=21
+a=21
+i=21
+
+#Q1
+# x= x+5
+x+=5
+print(x)
+
+#Q2
+# y -= 3
+y=y-3
+print (y)
+
+#Q3
+#z= z**2
+z**=2
+print (z)
+
+#Q4
+#w %=2
+w = w % 2
+print(w)
+
+#Q5
+# t = t/5
+t/=5
+print(t)
+
+#Q6
+# a = a*12
+a*=12
+print(a)
+
+#Q7
+#i//=7
+i= i//7
+print(i)
