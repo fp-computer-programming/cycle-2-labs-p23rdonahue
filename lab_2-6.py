0e44b97ac8e2490b3efecf6aef57396b92c9a864
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-09-20 13:24:21 -0400

    lab 2-6 completed

---
+++ lab_2-6.py
@@ -0,0 +1,24 @@
+# author: RED 9/20/22
+a=5
+b=10
+c=0
+d=-4
+
+if a % 2 == 0:
+    print('a is an even number')
+else: 
+    print('a is an odd number')
+
+if b % 2==0:
+    print('b is an even number')
+else: 
+    print('b is an odd number')
+if c % 2==0:
+    print('c is an even number')
+else: 
+    print('c is an odd number')
+
+if d % 2 ==0:
+    print('d is an even number')
+else: 
+    print('d is an odd number')
