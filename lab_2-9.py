1419295ff12dbb63819eee7f8f8391f000435dde
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-09-21 14:11:26 -0400

    lab 2-9 complete

---
+++ lab_2-9.py
@@ -0,0 +1,12 @@
+#author: RED 9/21/22
+
+Team_score=0
+
+if Team_score>=15:
+    print('you win a gold medal')
+elif Team_score>=12:
+    print('You win a silver medal')
+elif Team_score>=9:
+     print('you win a bronze medal')
+else:
+    print('no medal for you!!')
